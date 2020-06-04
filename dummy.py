0625252539cfb080fa2027e70fabd6648bac7ae6
Yogesh Dubey <d16@cybuzzsc.in>
2020-06-04 23:14:22 +0530

    minor changes

--- dummy.py
+++ dummy.py
@@ -14,8 +14,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 f = open('python.txt', 'r', errors = 'ignore')
 raw = f.read()
 raw = raw.lower() # convert to lowercase
-nltk.download('punkt') # first time use only
-nltk.download('wordnet') # first time use only
+#nltk.download('punkt') # first time use only
+#nltk.download('wordnet') # first time use only
 sent_tokens = nltk.sent_tokenize(raw)  # convert to list of sentences
 word_tokens = nltk.word_tokenize(raw)  # convert to list of words
 
@@ -56,7 +56,7 @@ def response(user_response) :
     req_tdidf = flat[-2]
 
     if(req_tdidf == 0) :
-        chatbot_response += 'I am sorry! I do not understant you.'
+        chatbot_response += "I am sorry! I don't understand you."
         return chatbot_response
     else :
         chatbot_response += sent_tokens[idx]
@@ -65,10 +65,10 @@ def response(user_response) :
 
 ## chatbot 
 flag = True
-print('Chatbot: I am chatbot. I will answer your queries about python. If you want to exit, type Bye!')
+print("robot: I'm robot & I'll answer your queries about Python & NLP. If you want to exit, type Bye!")
 
 while(flag == True) :
-    user_response = input()
+    user_response = input('human: ')
     user_response = user_response.lower()
     if(user_response != 'bye') :
         if(user_response == 'thanks' or user_response == 'thank you') :
@@ -76,14 +76,14 @@ while(flag == True) :
             print('Chatbot: You are welcome')
         else :
             if(greeting(user_response) != None) :
-                print('Chatbot: ', greeting(user_response))
+                print('robot: ', greeting(user_response))
             else :
-                print('Chatbot: ', end="")
+                print('robot: ', end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else :
         flag = False
-        print('Chatbot: Bye! take care..')
+        print('robot: Bye! take care..')
 
 
 
